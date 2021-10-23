aa93f649097ec75c3f8be465abf2167438d16a65
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2021-10-22 23:48:46 -0600

    added the base of the bottom navigation view

--- app/src/main/java/com/example/momentum/MainActivity.java
+++ app/src/main/java/com/example/momentum/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
     }
 }
