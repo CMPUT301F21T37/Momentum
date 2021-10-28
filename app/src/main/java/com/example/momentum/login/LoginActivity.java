8709d29eaa2ab443bad732865a91ef8ea61cbf5d
thestungyer <47674413+thestungyer@users.noreply.github.com>
2021-10-28 11:52:38 -0600

    loginScreen design

---
+++ app/src/main/java/com/example/momentum/login/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.momentum.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.momentum.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/momentum/login/SignUpActivity.java
@@ -0,0 +1,4 @@
+package com.example.momentum.login;
+
+public class SignUpActivity {
+}
