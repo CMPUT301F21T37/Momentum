b7e0d8ed06e40d34756fc23c2878e974e8335cb2
thestungyer <47674413+thestungyer@users.noreply.github.com>
2021-10-28 12:42:41 -0600

    changed Start screen

--- app/src/main/java/com/example/momentum/login/LoginActivity.java
+++ app/src/main/java/com/example/momentum/login/LoginActivity.java
@@ -1,19 +1,35 @@
 package com.example.momentum.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.momentum.MainActivity;
 import com.example.momentum.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton;
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
+        loginButton = findViewById(R.id.loginButton);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                // TODO: This is when the user is successful and logs in
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                LoginActivity.this.finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/momentum/login/SignUpActivity.java
+++ app/src/main/java/com/example/momentum/login/SignUpActivity.java
@@ -1,4 +1,15 @@
 package com.example.momentum.login;
 
-public class SignUpActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.momentum.R;
+
+public class SignUpActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_screen);
+    }
 }
