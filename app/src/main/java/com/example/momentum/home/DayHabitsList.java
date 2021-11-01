63f4e13d47ebc87136f9c43e2b03dc5725d19140
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2021-11-01 00:25:14 -0600

    finalized US 01.07.01

--- app/src/main/java/com/example/momentum/home/DayHabitsList.java
+++ app/src/main/java/com/example/momentum/home/DayHabitsList.java
@@ -1,6 +1,7 @@
 package com.example.momentum.home;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,19 +10,33 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 
 import com.example.momentum.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DayHabitsList extends ArrayAdapter<DayHabits> {
+    private static final String TAG = "DOCUMENT_EXISTENCE";
+
     private ArrayList<DayHabits> habits;
     private Context context;
+    private FirebaseFirestore db;
+    private boolean isHabitCompleted;
+    private String clickedDateStr;
 
-    public DayHabitsList(Context context, ArrayList<DayHabits> habits){
-        super(context,0, habits);
+    public DayHabitsList(Context context, ArrayList<DayHabits> habits, String clickedDateStr){
+        super(context, 0, habits);
         this.habits = habits;
         this.context = context;
+        this.clickedDateStr = clickedDateStr;
     }
 
     @NonNull
@@ -34,10 +49,46 @@ public class DayHabitsList extends ArrayAdapter<DayHabits> {
         }
 
         DayHabits habit = habits.get(position);
+        changeCardViewColorGivenHabitCompletion(habit.getDayHabitTitle(), view);
 
+        // sets the habit list names
         TextView habitTitle = view.findViewById(R.id.card_view_text);
         habitTitle.setText(habit.getDayHabitTitle());
 
         return view;
+    }
+
+    private void changeCardViewColorGivenHabitCompletion(String habit_title, View view) {
+        db = FirebaseFirestore.getInstance();
+        String habits_collection_name = "Habits";
+        String done_dates_collection_name = "Done dates";
+        CardView cardView = view.findViewById(R.id.card_view);
+
+        /*
+        How to check if a document exists in a collection
+        https://stackoverflow.com/questions/53332471/checking-if-a-document-exists-in-a-firestore-collection/53335711
+        Author: Alex Mamo
+        */
+        DocumentReference documentReference = db.collection(habits_collection_name).
+                document(habit_title).collection(done_dates_collection_name).document(clickedDateStr);
+
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        int redBackgroundColor = ContextCompat.getColor(context, R.color.red_main);
+                        cardView.setCardBackgroundColor(redBackgroundColor);
+                        Log.d(TAG, "Document exists!");
+                    } else {
+                        // do the default color in the xml file
+                        Log.d(TAG, "Document does not exist!");
+                    }
+                } else {
+                    Log.d(TAG, "Failed with: ", task.getException());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/momentum/home/HomeFragment.java
+++ app/src/main/java/com/example/momentum/home/HomeFragment.java
@@ -19,8 +19,15 @@ import com.example.momentum.R;
 import com.example.momentum.databinding.FragmentHomeBinding;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class HomeFragment extends Fragment {
+    public static final String DATE_TITLE_DAY_HABIT = "DATE_TITLE";
+    public static final String DAY_OF_WEEK = "DAY_OF_WEEK";
+    public static final String DATE_COMPARE_DAY_HABIT = "IS_DATE_CLICKED_CURRENT";
+    public static final String DATE_CLICKED_DAY_HABIT = "DATE_CLICKED";
+    public static final String DATE_CLICKED_DAY_HABIT_STR = "DATE_CLICKED_STR";
+
     private HomeViewModel HomeViewModel;
     private FragmentHomeBinding binding;
     private CalendarView calendar;
@@ -47,15 +54,20 @@ public class HomeFragment extends Fragment {
 
     private boolean onDateClick(CalendarView calendarView, int year, int month, int day) {
         // initialize Calendar format
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day);
+        Calendar calendar_click = Calendar.getInstance();
+        Calendar calendar_real = Calendar.getInstance();
+        calendar_click.set(year, month, day);
 
         // get the day of week for database query
-        String dayOfWeek = (String) DateFormat.format("EEEE", calendar);
+        String dayOfWeek = (String) DateFormat.format("EEEE", calendar_click);
 
         // get the complete date
-        String month_str = (String) DateFormat.format("MMM", calendar.getTime());
-        String date_title =  month_str + " " + day + " " + year + " Habit";
+        String month_str = (String) DateFormat.format("MMMM", calendar_click.getTime());
+        String date_title =  month_str + " " + day + ", " + year + " Habits";
+        String date = month_str + " " + day + ", " + year;
+
+        // compare if selected date and current date is the same for DayHabitsActivity
+        boolean isSame = compareDates(calendar_click, calendar_real);
 
         /*
         passing the data date to the day habits fragment via Bundle
@@ -63,12 +75,16 @@ public class HomeFragment extends Fragment {
         Author: Oum Saokosal
          */
         Bundle bundle = new Bundle();
-        bundle.putString("dateTitle", date_title);
-        bundle.putString("dayOfWeek", dayOfWeek);
+        bundle.putString(DATE_TITLE_DAY_HABIT, date_title);
+        bundle.putString(DAY_OF_WEEK, dayOfWeek);
+        bundle.putBoolean(DATE_COMPARE_DAY_HABIT, isSame);
+        bundle.putSerializable(DATE_CLICKED_DAY_HABIT, calendar_click.getTime());
+        bundle.putString(DATE_CLICKED_DAY_HABIT_STR, date);
 
         /*
         Android Developer Documentation
         using fragment transactions: https://developer.android.com/guide/fragments/transactions
+        Used to go to DayHabitsFragment
          */
         Fragment fragment = new DayHabitsFragment();
         fragment.setArguments(bundle);
@@ -78,6 +94,27 @@ public class HomeFragment extends Fragment {
         transaction.commit();
 
         return true;
+    }
+
+    /**
+     * It compares two dates if they are equal.
+     * @param clicked_date
+     * A Calendar instance of the clicked date by the user
+     * @param current_date
+     * A Calendar instance of the current date
+     * @return
+     * Returns either true or false of whether the two parameters are equal
+     */
+    private boolean compareDates(Calendar clicked_date, Calendar current_date) {
+        Date date_click = clicked_date.getTime();
+        Date date_current = current_date.getTime();
+
+        if (date_current.compareTo(date_click) == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     private boolean onHabitEventsClick(View view) {
