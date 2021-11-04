a772f8749f12c4ff212306abc6f1bac211776fab
Hyan320 <hyan3@ualberta.ca>
2021-11-04 12:25:52 -0600

    changed path to retrieve data from firebase

--- app/src/main/java/com/example/momentum/habitEvents/Indiv_habitEvent_view.java
+++ app/src/main/java/com/example/momentum/habitEvents/Indiv_habitEvent_view.java
@@ -1,20 +1,35 @@
 package com.example.momentum.habitEvents;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.momentum.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 /**
  * an activity show individual view of a habit event
@@ -24,8 +39,11 @@ import com.google.firebase.database.ValueEventListener;
  */
 public class Indiv_habitEvent_view extends AppCompatActivity {
     TextView title, he_date;
-    EditText comment;
-    DatabaseReference myRef;
+    EditText comments;
+    CollectionReference myRef;
+    private String uid;
+    private FirebaseUser user;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +52,71 @@ public class Indiv_habitEvent_view extends AppCompatActivity {
 
         title = (TextView) findViewById(R.id.habit_event);
         he_date = (TextView) findViewById(R.id.event_recordDate);
-        comment = (EditText) findViewById(R.id.comment);
+        comments = (EditText) findViewById(R.id.comment);
 
-        myRef = FirebaseDatabase.getInstance().getReference().child("Users").child("Habits").child("Events"); //not sure the correct path
-        myRef.addValueEventListener(new ValueEventListener() {
+        //get individual information from firebase
+        db = FirebaseFirestore.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        uid = user.getUid();
+        myRef = db.collection("Users").document(uid).collection("Habits");
+
+        //get name of habit_event
+        DocumentReference docRef = db.document("Coding");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String name = snapshot.child("title").getValue().toString();
-                String date = snapshot.child("date").getValue().toString();
-                String reason = snapshot.child("reason").getValue().toString();
-                title.setText(name);
-                he_date.setText(date);
-                comment.setText(reason);
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String name = document.getId();
+                        title.setText(name);
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
             }
+        });
 
+        //get date of habit_event
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String date = document.getString("date");
+                        he_date.setText(date);
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
 
+        //listen to comment
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Log.w(TAG, "Listen failed.", error);
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    String reason = snapshot.getString("comment");
+                    comments.setText(reason);
+                } else {
+                    Log.d(TAG, "Current data: null");
+                }
             }
         });
 
         //check if the comment has been changed
-        comment.addTextChangedListener(new TextWatcher() {
+        comments.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -68,7 +129,8 @@ public class Indiv_habitEvent_view extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                myRef.setValue(comment.getText().toString());
+                String changed = comments.getText().toString();
+                docRef.update("comment", changed);
             }
         });
     }
